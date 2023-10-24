5bcbf7156105e7e924c9ef0ea17ffa59523a9f5d
mizulk <1930764560@qq.com>
2023-10-24 15:15:20 +0800

    添加vueuse包，修改样式

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue";
+import { useDark, useToggle } from "@vueuse/core"
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
 </script>
 
 <template>
-	<h1>2023年软件配置课设</h1>
-	<HelloWorld msg="world"></HelloWorld>
+	<div>
+		<h1>2023年软件配置课设</h1>
+		<HelloWorld msg="world"></HelloWorld>
+		<el-button type="primary" @click="toggleDark()">Primary {{ isDark }}</el-button>
+	</div>
 </template>
 
 <style scoped lang="less"></style>
