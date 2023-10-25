498a9439c0628f4dc03714288e643f0bdcf9b6b6
mizulk <1930764560@qq.com>
2023-10-26 00:22:07 +0800

    marge

--- src/components/UserMainButton.vue
+++ src/components/UserMainButton.vue
@@ -2,7 +2,6 @@
 defineProps<{
 	text: string
 }>();
-<<<<<<< HEAD
 const emit = defineEmits<{
 	(event: 'click'): void
 }>();
@@ -10,12 +9,6 @@ const emit = defineEmits<{
 
 <template>
 	<div @click="emit('click')" class="user-main-btn">
-=======
-</script>
-
-<template>
-	<div>
->>>>>>> 825d5b5721ad9df496a53fe424268ace11a86971
 		<div>
 			<slot></slot>
 		</div>
@@ -24,7 +17,6 @@ const emit = defineEmits<{
 </template>
 
 <style scoped>
-<<<<<<< HEAD
 p {
 	margin-top: .2rem;
 	color: #b3b3b3;
@@ -42,10 +34,4 @@ p {
 		background: hsla(160, 100%, 37%, 0.2);
 	}
 }
-=======
-p{
-	margin-top: .2rem;
-	color: #b3b3b3;
-}
->>>>>>> 825d5b5721ad9df496a53fe424268ace11a86971
 </style>
