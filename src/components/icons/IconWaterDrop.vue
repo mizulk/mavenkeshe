b5d95cb2de78ddedb552390a3113d10fa80f23e0
mizulk <1930764560@qq.com>
2023-10-25 19:48:10 +0800

    新增加6个图标

---
+++ src/components/icons/IconWaterDrop.vue
@@ -0,0 +1,31 @@
+<template>
+	<!-- Generator: Adobe Illustrator 25.4.1, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+	<svg version="1.1" id="图层_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
+		y="0px" viewBox="0 0 32 32" style="enable-background:new 0 0 32 32;" xml:space="preserve">
+		<path class="st0" d="M15.9,32C8.7,32,4.3,27.9,4.3,21.3c0-6.9,10.2-20.2,10.7-20.8c0.4-0.6,1.3-0.7,1.9-0.3c0.1,0.1,0.2,0.2,0.3,0.3
+	c0.4,0.6,10.7,13.9,10.7,20.8C27.6,27.9,23.1,32,15.9,32z M15.9,3.5c-3.2,4.5-9,13.3-9,17.7c0,7.3,6.3,8,9,8s9-0.8,9-8
+	C24.9,16.9,19.2,8,15.9,3.5z" />
+		<line class="st1" x1="13.1" y1="22.1" x2="24.3" y2="22.1" />
+		<line class="st2" x1="19" y1="16.4" x2="19" y2="28" />
+	</svg>
+</template>
+
+<style scoped>
+.st0 {
+	fill: #79C3B9;
+}
+
+.st1 {
+	fill: none;
+	stroke: #79C3B9;
+	stroke-width: 2;
+	stroke-miterlimit: 10;
+}
+
+.st2 {
+	fill: none;
+	stroke: #79C2B9;
+	stroke-width: 2;
+	stroke-miterlimit: 10;
+}
+</style>
