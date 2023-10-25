28d9a34a71da4f10f487b4f6702659d50811136c
sanqian626 <3187465952@qq.com>
2023-10-25 14:47:03 +0800

    登录，注册

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,125 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import IconLogo from '../components/icons/InconLogo.vue'
+import { reactive } from 'vue'
+const loginForm = reactive({
+	userName: '',
+	password: ''
+});
+</script>
 
 <template>
-	<h1>LoginView</h1>
+	<div id="login-view">
+		<el-container>
+			<el-header class="login-header">
+				<span class="logo">LANSHU</span>
+				<IconLogo height="60"></IconLogo>
+			</el-header>
+			<el-main class="login-main">
+				<div class="left"></div>
+				<div class="right">
+					<el-form :model="loginForm" label-width="80px">
+						<h1 class="title">LANSHU小<span style="color: #9dd1e6;">蓝</span>书</h1>
+						<el-form-item>
+							<template #label>
+								<span class="form-label">账号：</span>
+							</template>
+							<el-input v-model="loginForm.userName" />
+						</el-form-item>
+
+						<el-form-item>
+							<template #label>
+								<span class="form-label">密码：</span>
+							</template>
+							<el-input v-model="loginForm.password" type="password" />
+						</el-form-item>
+						<el-form-item class="btn-warrper">
+							<el-button>登录</el-button>
+						</el-form-item>
+						<div class="a-warrper">
+							<RouterLink to="">还没有账号？快来注册一个吧！</RouterLink>
+						</div>
+					</el-form>
+				</div>
+			</el-main>
+			<el-footer></el-footer>
+		</el-container>
+	</div>
 </template>
 
-<style lang="less"></style>
+<style lang="less" scoped>
+.login-header {
+	height: 88px;
+
+	>.logo {
+		margin-left: 4rem;
+		font-size: 7rem;
+		font-weight: 500;
+		color: #5c341b;
+	}
+}
+
+.login-main {
+	padding: 0;
+	flex-grow: 1;
+	// background: #41a8a5;
+	background: #4fceca;
+	display: flex;
+
+	.left {
+		width: 60%;
+		background: url('../assets/login_image.png') no-repeat center;
+
+	}
+
+	.right {
+		display: flex;
+		justify-items: center;
+		align-items: center;
+		width: 40%;
+	}
+
+	// right
+	.el-form {
+		width: 32rem;
+		background-color: #5e341c;
+		padding: 4.4rem 2rem 1rem 3rem;
+		margin-left: 1.8rem;
+
+		.form-label {
+			color: white;
+			font-size: large;
+		}
+	}
+
+	.title {
+		font-size: 3rem;
+		text-align: center;
+		padding-bottom: 3.2rem;
+		text-decoration: underline;
+		color: white;
+	}
+
+	.btn-warrper {
+		padding-top: 2rem;
+		.el-button{
+			width: 20rem;
+		}
+
+	}
+
+	.a-warrper {
+		margin-top: 5rem;
+		height: 2rem;
+		color: #7fb7ca;
+		font-size: 1.6rem;
+		padding-bottom: 2rem;
+		text-decoration: underline;
+	}
+
+}
+
+#login-view {
+	display: flex;
+	height: 100vh;
+}
+</style>
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,96 @@
+<script setup lang="ts">
+import InconLogo from '@/components/icons/InconLogo.vue';
+import { reactive, ref } from 'vue'
+
+const registerFrom = reactive({
+	userName: '',
+	password: '',
+	password2: '',
+});
+const isChecked = ref(false);
+</script>
+
+<template>
+	<div id="register-view">
+		<el-container>
+			<el-header class="register-hader">
+				<div class="logo">
+					<InconLogo height="50"></InconLogo><span>欢迎注册</span>
+				</div>
+				<div class="login">
+					<span>已有账号？</span>
+					<RouterLink to="">请登录></RouterLink>
+				</div>
+			</el-header>
+			<!-- <hr> -->
+			<el-main class="register-main">
+				<el-form :model="registerFrom" label-width="100px">
+					<el-form-item label="账号名：">
+						<el-input v-model="registerFrom.userName" />
+					</el-form-item>
+					<el-form-item label="密码：">
+						<el-input v-model="registerFrom.password" type="password" />
+					</el-form-item>
+					<el-form-item label="确认密码：">
+						<el-input v-model="registerFrom.password2" type="password" />
+					</el-form-item>
+					<el-form-item>
+						<el-checkbox v-model="isChecked">
+							已阅读并同意以下协议<RouterLink to="">小蓝书平台服务协议</RouterLink>
+						</el-checkbox>
+					</el-form-item>
+					<el-form-item>
+						<el-button class="register-btn" color="#5c341b">
+							<span style="color: white;">注册</span>
+						</el-button>
+					</el-form-item>
+				</el-form>
+			</el-main>
+		</el-container>
+	</div>
+</template>
+
+<style lang="less" scoped>
+.register-hader {
+	height: 90px;
+	display: flex;
+	align-items: flex-end;
+	justify-content: space-between;
+	border-bottom: #e3e3e2 solid .7rem;
+
+	.logo>span {
+		display: inline-block;
+		font-size: 4rem;
+		margin-left: 1rem;
+		color: #5c341b;
+	}
+
+	.login {
+		font-size: 2.8rem;
+
+		>span {
+			color: #edddb2;
+		}
+	}
+}
+
+.register-main {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	.el-form {
+		width: 40%;
+	}
+
+	.register-btn {
+		width: 50rem;
+		color: #5c341b;
+	}
+}
+
+#register-view {
+	height: 100vh;
+	display: flex;
+}
+</style>
